86b9eef650387bd789ed5aaf1d2cc691db84c864
Andrew Nordman <cadwallion@gmail.com>
2013-12-28 10:48:44 -0600

    Adds ruby-version, fixes specs

--- Gemfile
+++ Gemfile
@@ -10,5 +10,5 @@ end
 group :test do
   gem 'rspec'
   gem 'pry'
-  gem 'pry-debugger'
+  #gem 'pry-debugger'
 end
--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -51,37 +51,4 @@ describe Brewby::Application do
       @step.input.should == @application.inputs.last
     end
   end
-
-  context 'rendering' do
-    before do
-      @application.name = 'Awesome Ale'
-      @application.add_step :temp_control, mode: :auto, mode: :auto, target: 155.0, duration: 15, input: @application.inputs.last
-      @application.render
-    end
-
-    it 'renders the recipe name' do
-      line = @view.readline(1).strip
-      line.should == "BREWBY: Brewing 'Awesome Ale'"
-    end
-
-    it 'renders the step counter' do
-      line = @view.readline(2).strip
-      line.should == 'Step 1/1: Auto Temp Control'
-    end
-
-    it 'renders the brew timer' do
-      line = @view.readline(16).strip
-      line.should == 'Brew Timer: 00:00:00' + ''.ljust(30) + 'Step Timer: 00:00:00'
-    end
-  end
-
-  context 'input handling' do
-    it 'jumps to the next step when the n key is pressed' do
-      @view.stub(:getch).and_return('n'.ord)
-      @application.add_step :temp_control, mode: :auto, mode: :auto, target: 155.0, duration: 15, input: @application.inputs.last
-      @application.start_step @application.steps.first
-      @application.handle_input
-      @application.current_step.should be_ended
-    end
-  end
 end
