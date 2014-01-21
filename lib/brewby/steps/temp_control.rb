972f5ccf7061ae96433643c4f82153ab182f05a8
Andrew Nordman <cadwallion@gmail.com>
2014-01-21 13:32:51 -0600

    Removes input handling from TempControl step

--- lib/brewby/steps/temp_control.rb
+++ lib/brewby/steps/temp_control.rb
@@ -106,52 +106,6 @@ module Brewby
       def duration_in_seconds
         @duration * 60
       end
-
-      def render(view)
-        view.move 2, 10
-        if @name
-          view.addstr @name.ljust(70)
-        else
-          view.addstr "#{mode.capitalize} Temp Control".ljust(70)
-        end
-
-        view.move 6, 0
-        view.addstr "Power Level: #{(power_level * 100.0).round(3)}%".ljust(70)
-        view.move 16, 50
-        view.addstr "Step Timer: #{timer_for(elapsed.to_i)}"
-        view.refresh
-
-        if target
-          view.move 4, 0
-          view.addstr "Target Temp: #{target} F".ljust(70)
-        end
-
-        view.move 5, 0
-        view.addstr "Actual Temp: #{@last_reading} F".ljust(25)
-
-        if threshold_reached
-          view.move 7, 0
-          view.addstr "Time Remaining: #{countdown_for(time_remaining)}".ljust(70)
-        else
-          view.move 7, 0
-          view.addstr "".ljust(70)
-        end
-      end
-
-      def handle_input key
-        case key
-        when 'e'.ord
-          if @mode == :manual
-            new_pct = [(power_level + 0.05).round(2), 1.0].min
-            set_power_level new_pct
-          end
-        when 'c'.ord
-          if @mode == :manual
-            new_pct = [(power_level - 0.05).round(2), 0.0].max
-            set_power_level new_pct
-          end
-        end
-      end
     end
   end
 end
